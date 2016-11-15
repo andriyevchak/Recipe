153a3af1ce2b8c45962d031140a72598db5116a4
ANDRIY\AndriyEvchak <pitbull666605@gmail.com>
2016-11-15 23:42:13 +0200

    Default MVC project

---
+++ Recipe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recipe.Startup))]
+namespace Recipe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
