eaa70be51462938b673db88b2e15b133c10473af
Andriy Evchak <pitbull666605@gmail.com>
2016-11-23 03:02:07 +0200

    Create All Recipewith image// and Update Doom Struvcture

---
+++ Recipe/Controllers/RecipeController/AllRecipeController.cs
@@ -0,0 +1,16 @@
+﻿using Recipe.Models.DbRecipe;
+using System.Web.Mvc;
+
+namespace Recipe.Controllers.RecipeController
+{
+    public class AllRecipeController : Controller
+    {
+        // GET: AllRecipe
+        public ActionResult AllRecipe()
+        {
+            RecipeContext db = new RecipeContext();
+            
+            return View(db.Recipes);
+        }
+    }
+}
---
+++ Recipe/Models/DbRecipe/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Recipe.Models.DbRecipe
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual List<IngredientAmount> Ingredients { get; set; }
+
+    }
+}
